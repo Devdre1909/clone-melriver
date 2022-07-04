dcff225e1233ba320f9664af59dc778dfd1ae536
Adegoke Temitope <adegoketemitope1909@gmail.com>
2022-07-04 09:16:47 +0100

    Updates

---
+++ precache-manifest.db75bfff7cd6b7142b2d7dc1d7772e26.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b539917ed126e9c5bae738fecf5e43f6",
+    "url": "/clone-melriver/index.html"
+  },
+  {
+    "revision": "316e4cab552663f4f7b7",
+    "url": "/clone-melriver/static/css/main.439903fd.chunk.css"
+  },
+  {
+    "revision": "5479bf99c46220d810b7",
+    "url": "/clone-melriver/static/js/2.8565c564.chunk.js"
+  },
+  {
+    "revision": "e400dab2eb40d60cf160b3e9f69cb19b",
+    "url": "/clone-melriver/static/js/2.8565c564.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "316e4cab552663f4f7b7",
+    "url": "/clone-melriver/static/js/main.82e7a5b3.chunk.js"
+  },
+  {
+    "revision": "0fa0c7005a675e870276",
+    "url": "/clone-melriver/static/js/runtime-main.e1213780.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/clone-melriver/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/clone-melriver/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/clone-melriver/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/clone-melriver/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "d187987d58f05a33d6cb592a4d7bdb88",
+    "url": "/clone-melriver/static/media/up-arrow-circle.d187987d.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/clone-melriver/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
